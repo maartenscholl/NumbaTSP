17413d332babf8df56654da2180db9acf5591898
Maarten Scholl <scholl.maarten@gmail.com>
2015-12-07 15:59:28 +0100

    Visualisation with matplotlib

--- sa/sa.py
+++ sa/sa.py
@@ -37,49 +37,50 @@ for i in range(V):
     result += distances[state[i], state[(1 + i) % V]]
 print('mu=', numpy.mean(distances), ' E[d]=', V * numpy.mean(distances), ' r=', result)
 
+
 # In[4]:
 import numba
 import numbapro
 from numbapro.cudalib import curand
 
 numba.cuda.profile_start()
-cycles = 1
+cycles = 10
 import optimise
 sa2opt = optimise.CreateKernel(V, cycles, 0.99)
 
 # precautions: 64/1024, 48/96
-threads = min(64+32, numba.cuda.get_current_device().MAX_THREADS_PER_BLOCK)
+threads = min(512, numba.cuda.get_current_device().MAX_THREADS_PER_BLOCK)
 blocks = 96 // 2
 operations = blocks * threads
 results = numpy.repeat(numpy.array([result], dtype=numpy.float32), operations)
 ###############################################################################
 iterations = 10000
-exponent = 0.7
-delta = 0.1
+initial_exponent = 0.7
+delta = 0.25
 
-best = (result, state, exponent)
+best = (result, state, initial_exponent)
+
 for i in range(iterations):    
-    configuration = best[1].reshape((1, V))
-    configuration = numpy.repeat(configuration, operations, axis=0)
-    
-    results = numpy.repeat(numpy.array([best[0]], dtype=numpy.float32), operations)
-
     exponent = best[2]
     high = numpy.max(0.000001, exponent + delta - 1./operations)
     low = numpy.max(0.000001, exponent - delta)
     temperatures = numpy.array(numpy.linspace(low, high, num=operations), dtype=numpy.float32, order='F')
 
+    configuration = best[1].reshape((1, V))
+    configuration = numpy.repeat(configuration, operations, axis=0)
+    results = numpy.repeat(numpy.array([best[0]], dtype=numpy.float32), operations)
+    d_distances = numba.cuda.to_device(distances)
+
     stream = numba.cuda.stream()
     with stream.auto_synchronize():
         entropy = operations * cycles * 3
         d_uniform = numba.cuda.device_array(entropy, dtype=numpy.float32, stream=stream)
-        random = curand.PRNG(curand.PRNG.MRG32K3A, stream=stream, seed=0)
-        random.uniform(d_uniform)
+        generator = curand.PRNG(curand.PRNG.MRG32K3A, stream=stream, seed=0)
+        generator.uniform(d_uniform)
 
         execute = sa2opt[(blocks, 1), (threads, 1), stream]
 
         d_results = numba.cuda.to_device(results, stream=stream)
-        d_distances = numba.cuda.to_device(distances, stream=stream)
         d_temperatures = numba.cuda.to_device(temperatures, stream=stream)
         d_configuration = numba.cuda.to_device(configuration, stream=stream)
         
@@ -89,18 +90,36 @@ for i in range(iterations):
         d_temperatures.to_host(stream=stream)
         d_configuration.to_host(stream=stream)
     
-    
     local = numpy.min(results)
     first = numpy.where(results == local)[0][0]
+    print((numpy.min(results),numpy.mean(results), exponent))
     sequence = configuration[first, :]
+    
     if local < best[0]:
-        best = (local, sequence, numpy.min(exponent * 0.99, temperatures[first]))
+        best = (local, sequence, numpy.min(exponent * 0.98, 0.9))#, temperatures[first]))
+        last = numpy.where(results == numpy.max(results))[0]
+        
+
     else:
-        best = (best[0], best[1], best[2] * 0.999)
-    print((numpy.min(results),numpy.max(results), best[2]))
-    results = numpy.repeat(numpy.array([local], dtype=numpy.float32), operations)
+        # should restart all
+        break
+
+###############################################################################
+import matplotlib.pyplot as plt
+
+figure = plt.figure()
+
+sequence = list(zip(*list(map(lambda v: vertices[v], best[1]))))
+plt.plot(sequence[0], sequence[1])
+plt.scatter(sequence[0], sequence[1])
+figure.show()
+
+
+
+
+
     
 numba.cuda.profile_stop()
 
-input("[fin]")
+input("exit>")
 sys.exit(0)
